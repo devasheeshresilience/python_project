1d54371d4b51422dd892b7e1ca18dfdaca27f86b
devasheeshresilience <devasheeshp@resiliencesoft.com>
2026-02-09 17:46:26 +0530

    feat: Update database configuration to use SQLite for development

--- fullstackcrud/settings.py
+++ fullstackcrud/settings.py
@@ -74,19 +74,29 @@ WSGI_APPLICATION = 'fullstackcrud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# MySQL database configuration for production
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'python_crud_db',
+#         'USER': 'root',
+#         'PASSWORD': 'root@123',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#         }
+#     }
+# }
+
+# SQLite3 database configuration for development
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'python_crud_db',
-        'USER': 'root',
-        'PASSWORD': 'root@123',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
