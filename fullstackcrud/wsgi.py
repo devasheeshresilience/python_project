c7600c2a4c8c6663098b567d4ece7f126eee3df8
devasheeshresilience <devasheeshp@resiliencesoft.com>
2026-02-09 18:05:27 +0530

    feat: Refactor WSGI config documentation and improve code organization

--- fullstackcrud/wsgi.py
+++ fullstackcrud/wsgi.py
@@ -7,10 +7,25 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
-import os
+"""
+WSGI config for fullstackcrud project.
 
-from django.core.wsgi import get_wsgi_application
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+# IMPORTANT: add your project directory to Python path
+path = '/home/resiliencesoft/python_project'
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fullstackcrud.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
